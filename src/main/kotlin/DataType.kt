39fb2e8abf769ba17bad1c11aa129ff8553ff700
Syairra Putri <asyairra22@gmail.com>
2022-10-04 15:44:37 +0700

    Latihan4Oktober22

---
+++ src/main/kotlin/DataType.kt
@@ -0,0 +1,13 @@
+fun main() {
+    val dataInt = 5
+    val dataString = "Hallo Syairra"
+    val dataBoolean = true
+
+    var dataIntVar = 10
+
+//  dataInt = 6
+    dataIntVar = 8
+
+    println("Ini data dengan val $dataInt")
+    println("Ini data dengan val $dataString")
+}
